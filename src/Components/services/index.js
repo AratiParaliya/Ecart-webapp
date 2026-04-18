0da98fd3ed70460df60889870b4b7fb348dff671
Arati <aparaliya757@rku.ac.in>
2026-04-18 22:31:57 +0530

    update

--- src/Components/services/index.js
+++ src/Components/services/index.js
@@ -1,52 +1,45 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import { fetchDataFromApi } from "../../utils/api";
+import { FaTruck, FaHeadset, FaCreditCard, FaUndo } from "react-icons/fa";
 
-   import { FaTruck, FaHeadset, FaCreditCard, FaUndo } from "react-icons/fa";
+const iconMap = {
+  FaTruck: <FaTruck />,
+  FaHeadset: <FaHeadset />,
+  FaCreditCard: <FaCreditCard />,
+  FaUndo: <FaUndo />
+};
+
 const Services = () => {
 
- 
-const serviceData = [
-  {
-    icon: <FaTruck />,
-    title: "Free Delivery",
-    subtitle: "On all orders",
-    bg: "rgb(253, 239, 230)"
-  },
-  {
-    icon: <FaHeadset />,
-    title: "24/7 Support",
-    subtitle: "Customer support",
-    bg: "rgb(206, 235, 233)"
-  },
-  {
-    icon: <FaCreditCard />,
-    title: "Secure Payment",
-    subtitle: "Safe checkout",
-    bg: "rgb(226, 242, 178)"
-  },
-  {
-    icon: <FaUndo />,
-    title: "Easy Return",
-    subtitle: "30 days return",
-    bg: "rgb(214, 229, 251"
-  }
-];
+  const [data, setData] = useState([]);
+
+useEffect(() => {
+  fetchDataFromApi("/api/services").then(res => {
+    const activeServices = (res.data || [])
+      .filter(item => item.status === true)
+      .slice(0, 4); // ✅ only latest 4
+
+    setData(activeServices);
+  });
+}, []);
+
   return (
-      <section className="services">
-             <div className='container'>
-      <div className="services-container">
-        {serviceData.map((item, index) => (
-          <div
-            className="service-box"
-            key={index}
-            style={{ backgroundColor: item.bg }}
-          >
-            <div className="icon">{item.icon}</div>
-            <h4>{item.title}</h4>
-            <p>{item.subtitle}</p>
-          </div>
-        ))}
-              </div>
-              </div>
+    <section className="services">
+      <div className="container">
+        <div className="services-container">
+
+          {data.map((item, i) => (
+            <div key={i} className="service-box" style={{ backgroundColor: item.bg }}>
+             <div className="icon">
+  {iconMap[item.icon] || <FaTruck />}
+</div>
+              <h4>{item.title}</h4>
+              <p>{item.subtitle}</p>
+            </div>
+          ))}
+
+        </div>
+      </div>
     </section>
   );
 };
